436b4a317c5bf6f6502980b9c99104533cfb4aeb
Charlie <charlie_jb@hotmail.co.uk>
2021-09-26 11:47:56 +0100

    first commit

---
+++ src/main/java/com/charliebignell/app/App.java
@@ -0,0 +1,13 @@
+package com.charliebignell.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
