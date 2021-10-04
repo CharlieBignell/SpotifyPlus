7ed08e9dc4ddc3dbe0d49435d37ab034bd2701d5
Charlie <charlie_jb@hotmail.co.uk>
2021-10-04 20:22:47 +0100

    Finished song class and tests

--- src/main/java/com/charliebignell/app/Song.java
+++ src/main/java/com/charliebignell/app/Song.java
@@ -3,41 +3,46 @@ package com.charliebignell.app;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Song 
-{
+public class Song {
 
-    private String name = null;
-    private String artist = null;
+    private final String name;
+    private final String artist;
     private List<String> tags = new ArrayList<String>();
-    
 
-    public Song(String name, String artist){
+    public Song(String name, String artist) throws IllegalArgumentException {
         super();
+        if (name == null || artist == null) {
+            throw new IllegalArgumentException("Missing name or artist");
+        }
         this.name = name;
         this.artist = artist;
     }
 
-    public String getName(){
+    public String getName() {
         return this.name;
     }
 
-    public String getArtist(){
+    public String getArtist() {
         return this.artist;
     }
 
-    public String getFormattedSong(){
+    @Override
+    public String toString() {
         return this.name + ", by " + this.artist;
     }
 
-    public void addTag(String tag){
+    public void addTag(String tag) {
         this.tags.add(tag);
-    }    
+    }
 
     public void removeTag(String tag){
-        tags.remove(tag);
+        if(containsTag(tag)){
+            tags.remove(tag);
+        }
     }
 
-    public void getTags(){
-        
+    public boolean containsTag(String tag) {
+        return this.tags.contains(tag);
     }
+
 }
--- src/test/java/com/charliebignell/app/SongTest.java
+++ src/test/java/com/charliebignell/app/SongTest.java
@@ -3,20 +3,22 @@ package com.charliebignell.app;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-public class MainTest {
+public class SongTest {
     private Song song = new Song("name", "artist");
 
     @Test
     public void songGetters() {
         assertEquals("artist", song.getArtist());
         assertEquals("name", song.getName());
-        assertEquals("name, by artist", song.getFormattedSong());
+        assertEquals("name, by artist", song.toString());
     }
 
     @Test
     public void addAndRemoveTagsToSongs() {
         song.addTag("tag");
-        
+        assertEquals(song.containsTag("tag"), true);
+        song.removeTag("tag");
+        assertEquals(song.containsTag("tag"), false);
     }
 
 }
